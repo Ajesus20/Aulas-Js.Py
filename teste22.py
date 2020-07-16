dc20c82b1e2dbd2c45e82f82a5915ec8345bd7c6
Jesus <ajesus@deloitte.com>
2020-07-16 19:49:10 -0300

    Fechou

---
+++ teste22.py
@@ -0,0 +1,6 @@
+valor = int(input("Informe o valor que será multiplicado na tabuada: "))
+multiplicador = 0
+for tabuada in range(multiplicador, 10) = 
+    multiplicador  = multiplicador +1
+    print(f"{valor} x {multiplicador} = {valor x multiplicador}")
+print("Fim")
